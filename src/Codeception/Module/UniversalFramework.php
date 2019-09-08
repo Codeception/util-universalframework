72394302efd0cb7e581c22d5fb8607816367a2ff
Gintautas Miselis <gintautas@miselis.lt>
2019-09-08 18:39:21 +0300

    Allow setting index in module configuration

--- src/Codeception/Module/UniversalFramework.php
+++ src/Codeception/Module/UniversalFramework.php
@@ -1,13 +1,22 @@
 <?php
 namespace Codeception\Module;
 
-class UniversalFramework extends \Codeception\Lib\Framework
+use Codeception\Configuration;
+use Codeception\Lib\Connector\Universal as Connector;
+use Codeception\Lib\Framework;
+
+class UniversalFramework extends Framework
 {
-    public function __construct()
+
+    public function _initialize()
     {
-        $index = '/app/index.php';
-        $this->client = new \Codeception\Lib\Connector\Universal();
-        $this->client->setIndex(\Codeception\Configuration::dataDir().$index);
+        if (isset($this->config['index'])) {
+            $index = $this->config['index'];
+        } else {
+            $index = Configuration::dataDir() . '/app/index.php';
+        }
+        $this->client = new Connector();
+        $this->client->setIndex($index);
     }
 
     public function useUniversalFramework()
